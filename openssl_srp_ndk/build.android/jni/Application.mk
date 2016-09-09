13bebd6b05fac6f5c2cf0a231a9fcac60efecc1a
ouyangchengfeng <ouyangchengfeng@xiaomi.com>
2016-09-09 10:39:26 +0800

    mv openssl_srp_ndk to openssl_srp
    Signed-off-by: ouyangchengfeng <ouyangchengfeng@xiaomi.com>

--- openssl_srp_ndk/build.android/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-#APP_ABI := armeabi armeabi-v7a mips x86
-#APP_ABI := armeabi
-#APP_API := arm64-v8a
-#APP_API := all
-#APP_API := x86
-#APP_ABI := arm64-v8a
-APP_ABI := armeabi
-APP_PLATFORM  := android-21
